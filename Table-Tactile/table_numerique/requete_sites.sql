3d2530e9df4fc06e1fb194880e504f98ce93c553
Charles Berdaguer <charles@macbook-pro-de-charles.home>
2018-03-13 15:05:31 +0100

    maj requetes

--- Table-Tactile/table_numerique/requete_sites.sql
+++ Table-Tactile/table_numerique/requete_sites.sql
@@ -4,3 +4,9 @@ ON (gestion_table_table.id = gestion_table_table_site.table_id)
 INNER JOIN site_web_site
 ON(site_web_site.id = gestion_table_table_site.site_id)
 WHERE (gestion_table_table_site.table_id = 2);
+
+
+SELECT * FROM galerie_photo_photo
+INNER JOIN galerie_photo_theme
+ON (galerie_photo_photo.theme_id=galerie_photo_theme.id)
+WHERE(galerie_photo_theme.nom = 'perpignan')
